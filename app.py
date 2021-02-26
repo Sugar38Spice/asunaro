7a8ab4e963871af186eba65572a185e4326faa23
Ryuto Hokama <lute816@gmail.com>
2021-02-26 16:01:54 +0900

    div tag

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask , render_template , request , redirect , session
 
 app = Flask(__name__)
 
-@app.route("/logout")
+@app.route("/index")
 def logout():
     session.pop('user_id',None)
     # ログアウト後はログインページにリダイレクトさせる
-    return redirect("/login")
+    return redirect("/index")
 
 
-  if __name__ == "__main__":
+
+if __name__ == "__main__":
     # Flask が持っている開発用サーバーを、実行します。
     app.run()
