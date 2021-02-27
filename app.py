e86fbafecef5670bb5fb99163cbc4f1608c9a064
Aki-Miyazaki <miyazaki.ak23@gmail.com>
2021-02-27 10:20:43 +0900

    routeとHTML紐付けできないいかいじった

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def top():
     return render_template("index.html")
 
-@app.route("/",methods=["POST"])
+@app.route("/login")
 def move_login():
     return render_template("login.html")
 
@@ -39,16 +39,20 @@ def login_post():
 
 
 @app.route("/myp")
-def template():
-    py_name = "name"
+def my_page():
+    # py_name = "name"
+    return render_template("myp.html")
     # return name + "さん、こんにちは"
-    return render_template("myp.html", name = py_name)
+    # return render_template("myp.html", name = py_name)
 
 
-# @app.route("/temptest")
-# def temptest():
-#     py_name = "にんじゃわんこ"
-#     return  render_template("index.html", user_name = py_name)
+@app.route("/zzz")
+def zzz_page():
+    return render_template("zzz.html")
+
+@app.route("/grouth")
+def grouth_page():
+    return render_template("grouth.html")
 
 
 @app.route("/staff")
@@ -194,7 +198,6 @@ def regist_post():
 def logout():
     session.pop("user_id",None)
     return  render_template("login.html")
-
 
 
 
