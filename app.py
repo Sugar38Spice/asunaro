3b61c62a2e075ee60349e8dcc46dc72fcea33b60
Ryuto Hokama <lute816@gmail.com>
2021-02-27 19:43:45 +0900

    newページ

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from datetime import datetime
 def index():
     return render_template("index.html")
 
+
+
 @app.route("/login",methods=["POST"])
 def move_login():
     return render_template("login.html")
