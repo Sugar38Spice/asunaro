0696cf04ed99967b98d20961f38f9dcbba0b21a3
Ryuto Hokama <lute816@gmail.com>
2021-03-01 16:48:19 +0900

    pythonの画面遷移

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ from datetime import datetime
 def index():
     return render_template("index.html")
 
+# ページ遷移
+@app.route("/login",methods=["POST"])
+def login():
+    return render_template("login.html")
 
 
 
